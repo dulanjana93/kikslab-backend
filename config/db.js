e0e7fe212162f286c47bf91dc2129357fe633454
Dulanjana Maduwantha <133597258+dulanjana93@users.noreply.github.com>
2025-09-06 21:52:18 +0530

    Update db.js
    change codes for deploy railway

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
-try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
-  console.log("Database Connected Successfully");
-} catch (err) {
-  console.log("Database Not Connected");
-}
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database Connected Successfully");
+  } catch (err) {
+    console.log("Database Not Connected:", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
